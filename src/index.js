10ced93f10cb229fcbf9c2412ea5d63fb7e3251d
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2022-09-02 17:55:46 -0300

    first routes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  users.push({ 
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  });
+
+  return response.status(201).json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.header;
+
+  const user = users.find( (user) => user.username === username);
+
+  if (!user) {
+      return response.status(400).json({ error: "User not found"});
+  }
+
+  request.user = user;
+
+  return response.json(users.user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.header;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline,
+    create_at: new Date()
+}
+  
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
